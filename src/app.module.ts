061c5799ca2daa42902ed917bfb096ffa3d8e355
Iago-Ruas <vtzard@gmail.com>
2024-03-04 19:27:27 -0300

    feat(setup): loans resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
+import { LoansModule } from './loans/loans.module';
 
 @Module({
-  imports: [UsersModule, BooksModule, AuthModule],
+  imports: [UsersModule, BooksModule, AuthModule, LoansModule],
   controllers: [],
   providers: [],
 })
