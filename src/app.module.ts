6455fc6fdbb0f888f862f4a34e8d073a3d56a930
Iago-Ruas <vtzard@gmail.com>
2024-03-04 18:32:29 -0300

    feat(setup): modules initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, BooksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
