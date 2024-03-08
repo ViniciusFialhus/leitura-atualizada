e61083c372d1911f9d3b52b85fc507738567780c
Luiz D O Santos <lzdavi1325@gmail.com>
2024-03-08 16:01:03 -0300

    feat(auth): autenticação implementada

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,1 +1,14 @@
-export class AuthController {}
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserLoginDto } from './dtos/user-login.dto';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() user: UserLoginDto) {
+    return await this.authService.login(user);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { User } from '../users/entities/user.entity';
+import { UserLoginDto } from './dtos/user-login.dto';
+import { UserPayload } from './models/UserPayload';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaClient,
+  ) { }
+
+  async login(userLogin: UserLoginDto): Promise<UserToken> {
+    const user = await this.findByEmail(userLogin.email);
+
+    const isPasswordValid = await bcrypt.compare(
+      userLogin.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Email ou senha incorretos.');
+    }
+
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+    };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+  private async findByEmail(email: string): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Email não encontrado');
+    }
+
+    return user;
+  }
+
+  async validateUser(userPayload: UserPayload): Promise<User> {
+    const user = this.prisma.user.findUnique({
+      where: {
+        id: userPayload.sub,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
+
