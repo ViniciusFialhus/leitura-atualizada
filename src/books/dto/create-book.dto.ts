2c8a1dafa4442f4f8549ea613397474cae2353da
Iago-Ruas <vtzard@gmail.com>
2024-03-08 16:54:59 -0300

    feat(book.dto): added Book DTO for data transfer

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,47 @@
-export class CreateBookDto {}
+import {
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateBookDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  title: String;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  author: String;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(10)
+  @MaxLength(13)
+  isbn: String;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsUrl()
+  imageUrl: String;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(20)
+  genre: String;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  description: String;
+
+  @IsOptional()
+  @IsString()
+  @IsDate()
+  publishedAt: Date;
+}
