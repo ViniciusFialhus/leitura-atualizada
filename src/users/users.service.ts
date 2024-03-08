2ad207fdaa770d76696e755c42ceaca25b796141
ViniciusFialhus <contatoviniciusfialhu@gmail.com>
2024-03-08 14:42:44 -0300

    fix(router [user]): realizado a correção da tipagem de id

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,15 +12,15 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  update(id: string, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} user`;
   }
 }
