3ab7d8f22332609d8db42dd7fc259b203037e05d
Luiz D O Santos <lzdavi1325@gmail.com>
2024-03-08 15:58:50 -0300

    feat(auth): adiciona estrategia jwt para autenticação

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserFromJwt } from '../models/UserFromJwt';
+import { UserPayload } from '../models/UserPayload';
+import { AuthService } from './../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+  async validate(payload: UserPayload): Promise<UserFromJwt> {
+    const user = await this.authService.validateUser(payload);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return {
+      id: user.id,
+      email: user.email
+    };
+  }
+}
