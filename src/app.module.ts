b10aed66e9c7328c5d6d3fce9e86449a9958e0ea
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-03-11 15:06:24 -0300

    fix(auth) - restaurei pasta auth por questão de merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
 import { LoansModule } from './loans/loans.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, BooksModule, LoansModule],
+  imports: [UsersModule, BooksModule, LoansModule, AuthModule],
   controllers: [],
   providers: [],
 })
