86c3a8e8b71a6b551b870fd3b28056a49a12a773
ViniciusFialhus <contatoviniciusfialhu@gmail.com>
2024-03-12 18:51:03 -0300

    fix(auth): realizada a restauração da pasta auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,47 @@
+import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Req,
+  Res,
+  UseGuards,
+  Body,
+  HttpCode,
+  Post,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserLoginDto } from './entities/user-login.dto';
+import { Response } from 'express';
+import { GoogleOauthGuard } from './guards/google-oauth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() user: UserLoginDto) {
+    return await this.authService.login(user);
+  }
+
+  @Get('google')
+  @UseGuards(GoogleOauthGuard)
+  async auth() {}
+
+  @Get('google/callback')
+  @UseGuards(GoogleOauthGuard)
+  async googleAuthCallback(@Req() req, @Res() res: Response) {
+    const token = await this.authService.login(req.user);
+
+    res.cookie('access_token', token, {
+      maxAge: 2592000000,
+      sameSite: true,
+      secure: false,
+    });
+
+    return res.status(HttpStatus.OK);
+  }
+
+}
