e5376e4bd3f78684d079a8ace36287f9dd561611
Iago-Ruas <vtzard@gmail.com>
2024-03-14 13:20:03 -0300

    Feat (Loans): Dto Created.

--- src/loans/dto/create-loan.dto.ts
+++ src/loans/dto/create-loan.dto.ts
@@ -1,1 +1,10 @@
-export class CreateLoanDto {}
+import { IsUUID } from 'class-validator';
+import { Loan } from '../entities/loan.entity';
+
+export class CreateLoanDto implements Loan {
+  @IsUUID('all', { message: 'Must be a valid UUID' })
+  userId: String;
+
+  @IsUUID('all', { message: 'Must be a valid UUID' })
+  bookId: String;
+}
