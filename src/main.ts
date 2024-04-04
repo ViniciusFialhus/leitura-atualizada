f6fde72810f9e22339cb0e194711550d164a70a6
Luiz D O Santos <lzdavi1325@gmail.com>
2024-04-04 15:34:49 -0300

    adds swagger setup initial

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Leitura Atualizada')
+    .setDescription('Documentação API leitura atualizada')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(4000);
 }
 bootstrap();
