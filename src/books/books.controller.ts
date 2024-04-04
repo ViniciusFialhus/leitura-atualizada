1995b58af2ee2fbc4cfc5b4703c898e6954fcee1
Alexon Granja <alexongranja@gmail.com>
2024-04-04 20:57:04 -0300

    feat(guards): Database change giving a loan status field to books and route guards implemented thoughout project

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,40 +1,55 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 import { Book } from './entities/book.entity';
+import { AuthenticatedUserGuard } from 'src/auth/guards/authenticated-user.guard';
+import { AdminAccessGuard } from 'src/auth/guards/admin.guard';
 
 @Controller('books')
 export class BooksController {
-  constructor(private readonly booksService: BooksService) { }
+  constructor(private readonly booksService: BooksService) {}
 
   @Post()
+  @UseGuards(AuthenticatedUserGuard, AdminAccessGuard)
   createBook(@Body() createBookDto: CreateBookDto) {
-    return this.booksService.create(createBookDto)
+    return this.booksService.create(createBookDto);
   }
 
-  @Get()
+  @Get('all')
   findAllBook() {
-    return this.booksService.findAll()
+    return this.booksService.findAll();
   }
 
   @Get('search')
   searchBook(@Query() query: Book) {
-    return this.booksService.searchBook(query)
+    return this.booksService.searchBook(query);
   }
 
   @Get(':id')
   findOneBook(@Param('id') id: string) {
-    return this.booksService.findOne(id)
+    return this.booksService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthenticatedUserGuard, AdminAccessGuard)
   updateBook(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.booksService.update(id, updateBookDto)
+    return this.booksService.update(id, updateBookDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthenticatedUserGuard, AdminAccessGuard)
   removeBook(@Param('id') id: string) {
-    return this.booksService.remove(id)
+    return this.booksService.remove(id);
   }
 }
