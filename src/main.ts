7f6d6dde30451172ad6a1b9277abb16cc20a3712
Luiz D O Santos <lzdavi1325@gmail.com>
2024-04-05 18:06:13 -0300

    docs: add swagger bearerAuth settings

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,14 @@ async function bootstrap() {
     .setTitle('Leitura Atualizada')
     .setDescription('Documentação API leitura atualizada')
     .setVersion('1.0')
+    .addBearerAuth({
+      type: "http",
+      scheme: "bearer",
+      bearerFormat: "JWT",
+      description: "Enter token",
+      name: "Authorization",
+      in: "header"
+    }, "KEY_AUTH")
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
